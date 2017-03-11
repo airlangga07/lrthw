9585b9b5b9cd8c95d5426c5d4a9723c2ee4d0a25
Mikael Airlangga Trianggoro <mikael.airlangga@gmail.com>
2017-03-11 22:51:37 +0800

    Exercise 1 - 10

---
+++ exercise_1-10/ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Mikael Airlangga'
+my_age = 27
+my_height = 74
+my_weight = 180
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get exactly right
+puts "If i add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
